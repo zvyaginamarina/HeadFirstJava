8907a20df19d6cbb995a6e6e12e831e78ce9b100
zvyaginamarina <admzv@yandex.ru>
2025-10-16 08:58:45 +0400

    update some classes

--- DotComGame.java
+++ DotComGame.java
@@ -1,90 +1,63 @@
 import java.util.ArrayList;
 
 public class DotComGame {
-//Объявляем и инициализируем переменные, которые нам понадобятся
     GameHelper helper = new GameHelper();
     int numOfGuesses = 0;
     ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
            
    void setUpGame(){
 
-//Создаем три объекта DotCom, даем им имена и помещаем в ArrayList
         DotCom theDotCom1 = new DotCom();
         DotCom theDotCom2 = new DotCom();
         DotCom theDotCom3 = new DotCom();
 
-        theDotCom1.siteName = ("Macro");
-        theDotCom2.siteName = ("Tops Daily");
-        theDotCom3.siteName = ("Big C");
+        theDotCom1.siteName("Macro");
+        theDotCom2.siteName("Tops Daily");
+        theDotCom3.siteName("Big C");
 
         dotComList.add(theDotCom1);
         dotComList.add(theDotCom2);
         dotComList.add(theDotCom3);
     
-//Выводим краткие инструкции
         System.out.println("Your goal is to drown " + dotComList.size() + "sites");
         System.out.println("theDotCom1.siteName, theDotCom2.siteName, theDotCom3.siteName");
         System.out.println("Try to drown them with fewer hits");
 
-//Повторяем с каждым объектом DotCom в списке
         for (DotCom dotComToSet : dotComList){
-
-//Запрашиваем у вспомогательного объекта адрес "сайта"
             ArrayList<String> newLocation = helper.placeDotCom(3);
-            
-//Вызываем сеттер из объекта DotCom, что бы передать ему местоположение
             dotComToSet.setLocationCells(newLocation);
         }
     };
 
    void startPlaying(){
 
-//До тех пор, пока список объектов DotCom не станет пустым
-    while(dotComList.isEmpty()){
-
-//Получаем пользовательский ввод
+    while(!dotComList.isEmpty()){
     String userGuess = helper.getUserInput("Input number");
-
-//Вызываем метод checkUserGuess
     checkUserGuess(userGuess);
     };
 
-//Вызываем метод finishGame
     finishGame();
    };
 
-   void checkUserGuess(){  
-
-//Инкрементируем количество попыток, которые сделал пользователь
+public void checkUserGuess(String userGuess){  
     numOfGuesses++;
-
-//Подразумеваем промах, пока не выяснили обратного
     String result = "Off target";
-
-//Повторять для всех объектов DotCom в списке
-   for (DotCom dotComToTest : dotComList){
-
-//Просим DotCom проверить ход пользователя, тщем попадание или потопление
+    for (DotCom dotComToTest : dotComList){
     result = dotComToTest.checkYourself(userGuess);
      if (result.equals("Hitted")) {
-
-//Выбираемся из цикла раньше времени
         break;
      }
      if (result.equals("Drowned")) {
-
-//Ему пришел конец, удаляем его из списка "сайтов" и выходим из цикла
         dotComList.remove(dotComToTest);
         break;
      }  
     }
-//Выводим для пользователя результат
+
      System.out.println(result);
 }
 
    void finishGame(){
 
-//Выводим сооющение о том, как пользователь прошел игру
     if (numOfGuesses <= 5){
         System.out.println("Congratulations! You win with just " + numOfGuesses + " hits!");
     } else {
@@ -96,13 +69,8 @@ public class DotComGame {
 
    public static void main(String[] args) {
 
-//Создаем игровой объект
     DotComGame game = new DotComGame();
-
-//Говорим игровому объекту подготовить игру
     game.setUpGame();
-
-//Говорим игровому объекту начать главный игровой цикл
     game.startPlaying();
    }
 };
--- GameHelper.java
+++ GameHelper.java
@@ -1,6 +1,13 @@
 import java.io.*;
+import java.util.*;
 
 public class GameHelper {
+    private static final String alphabets = "abcdefg";
+    private int gridLength = 7;
+    private int gridSize = 49;
+    private int [] grid = new int[gridSize];
+    private int comCount = 0;
+
     public String getUserInput(String prompt) {
         String inputLine = null;
         System.out.print(prompt + " ");
@@ -11,10 +18,70 @@ public class GameHelper {
         } catch (IOException e) {
             System.out.println("IOException: " + e);
         };
-        return inputLine;
+        return inputLine.toLowerCase();
     };
 
-void placeDotCom(){
+public ArrayList<String> placeDotCom(int comSize){
+ArrayList<String> alphaCells = new ArrayList<String>();
+// String [] alphacoords = new String[comSize];
+String temp = null;
+int [] coords = new int[comSize];
+int attempts = 0;
+boolean success = false;
+int location = 0;
+
+comCount++;
+int incr = 1;
+if ((comCount % 2) == 1){
+    incr = gridLength;
+}
+
+while (!success && attempts++ < 4) {
+    location = (int) (Math.random() * gridSize);
+    System.out.println("try " + location);
+    int x = 0;
+    success = true;
+    while (success && x < comSize){
+        if (grid[location] == 0){
+            coords[x++] = location;
+            location += incr;
+            if (location >= gridSize) {
+            success = false;
+            }
+        if (x>0 && (location % gridLength == 0)){
+            success = false;
+        }
+        else {
+            System.out.println("using " + location);
+            success = false;
+        }
+        }
+    }
+
+    x = 0;
+    int row = 0;
+    int column = 0;
+    System.out.println("\n");
+    while (x < comSize){
+        grid[coords[x]] = 1; 
+        row = (int) (coords[x] / gridLength);
+        column = coords[x] % gridLength;
+        temp = String.valueOf(alphabets.charAt(column));
+
+        alphaCells.add(temp.concat(Integer.toString(row)));
+        x++;
+        System.out.println(" coord " + x + " = " + alphaCells.get(alphaCells.size() - 1));
+    } 
 
 };
-}
+    System.out.println("\n");
+    return alphaCells;
+};
+};
+
+// class TestHelper{
+//     public static void main(String[] args) {
+//         GameHelper helper1 = new GameHelper();
+//         helper1.placeDotCom(3);
+//     }
+// }
